0e5a8a45b92d98dd7f9e5d15acd2f143b7b1c442
jlinear <jyang9@nd.edu>
2018-02-26 16:02:42 -0500

    fix uuid format error

--- app/src/main/java/nd/edu/bluenet_new/BleService.java
+++ app/src/main/java/nd/edu/bluenet_new/BleService.java
@@ -51,7 +51,7 @@ public class BleService extends Service {
     public final static String EXTRA_DATA =
             "com.example.bluetooth.le.EXTRA_DATA";
 
-    public final static UUID UUID_HEART_RATE_MEASUREMENT = MainActivity.TEST_SERVICE
+//    public final static UUID UUID_HEART_RATE_MEASUREMENT = MainActivity.TEST_SERVICE;
 
 
     // Implements callback methods for GATT events that the app cares about.  For example,
@@ -114,7 +114,7 @@ public class BleService extends Service {
         // This is special handling for the Heart Rate Measurement profile.  Data parsing is
         // carried out as per profile specifications:
         // http://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
-        if (UUID_HEART_RATE_MEASUREMENT.equals(characteristic.getUuid())) {
+        if (MainActivity.TEST_SERVICE.equals(characteristic.getUuid())) {
             int flag = characteristic.getProperties();
             int format = -1;
             if ((flag & 0x01) != 0) {
@@ -285,7 +285,7 @@ public class BleService extends Service {
         mBluetoothGatt.setCharacteristicNotification(characteristic, enabled);
 
         // This is specific to Heart Rate Measurement.
-        if (UUID_HEART_RATE_MEASUREMENT.equals(characteristic.getUuid())) {
+        if (MainActivity.TEST_SERVICE.equals(characteristic.getUuid())) {
             BluetoothGattDescriptor descriptor = characteristic.getDescriptor(
                     UUID.fromString(MainActivity.CLIENT_CHARACTERISTIC_CONFIG));
             descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
